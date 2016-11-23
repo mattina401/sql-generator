6a53403248cf68a5ffc017c3907df6df379b240c
mattina401 <mattina401@gmail.com>
2016-11-23 00:06:13 -0800

    switch to angularJs
    add table, add column, delete table, delete column

--- app/app.js
+++ app/app.js
@@ -1,16 +1,149 @@
-var app = angular.module("UserAuthTutorial", ["ui.router"]);
+var app = angular.module("myApp", ['ngDragDrop']);
 
-app.config(function ($stateProvider) {
-    $stateProvider
-        .state("login", {
-            url: "/",
-            controller: "LoginController",
-            templateUrl: "login.html"
-        })
-        .state("application", {
-            url: "/app",
-            controller: "MainController",
-            templateUrl: "application.html"
-        })
+var tableList = [];
+var columnList =[];
+var obj = {};
 
+app.controller('mainController', function ($scope) {
+
+    $scope.createTable = function (tName) {
+
+        // check table name already exists or not
+        if ($.inArray(tName, tableList) > -1) {
+            alert(tName + " already exists")
+        } else if(tName === undefined) {
+            alert("please type table name")
+        }
+        else {
+            // add table name to tableList
+            tableList.push(tName);
+            //console.log(tableList);
+
+            obj[tName] = {
+                //tableName: tName
+            };
+        }
+    }
+
+    $scope.deleteTable = function(tName) {
+
+        // delete table from table list
+        var idx = tableList.indexOf(tName);
+
+        if (idx > -1) {
+            tableList.splice(idx, 1);
+        }
+
+        // delete table from obj
+        delete obj[tName];
+
+    }
+
+    $scope.deleteColumn = function(tName, cName) {
+        //console.log(cName);
+        //console.log(obj);
+        delete obj[tName][cName];
+
+        // delete column from column list
+        var idx = columnList.indexOf(tName+cName);
+
+        if (idx > -1) {
+            columnList.splice(idx, 1);
+        }
+
+        console.log(obj);
+
+    }
+
+
+    $scope.displayTable = function () {
+        $scope.tableList = tableList;
+    }
+
+    $scope.displayColumn = function(tName) {
+
+        return obj[tName];
+    }
+
+    $scope.displayTable();
+
+});
+
+
+// send table Name to share modal
+app.controller('sendController', ['$scope', 'dataShare',
+    function ($scope, dataShare) {
+        $scope.send = function (tName) {
+            dataShare.sendData(tName);
+        };
+    }
+]);
+
+app.controller('colController', ['$scope', '$http', '$window', 'dataShare',
+
+    function ($scope, $http, $window, dataShare) {
+
+        $scope.tableType = ['INT', 'CHARACTER', 'VARCHAR'];
+        $scope.attributes = ['BINARY', 'UNSIGNED', 'UNSIGNED ZEROFILL', 'on update CURRENT_TIMESTAMP', ''];
+        $scope.indexType = ['PRIMARY', 'UNIQUE', ''];
+
+        //variables
+        $scope.tableName = undefined;
+
+        $scope.columnInfo = {
+            cName: undefined,
+            typeList: undefined,
+            length: undefined,
+            attributesList: undefined,
+            nullCKBox: false,
+            indexList: undefined,
+            aiCKBox: false
+        }
+
+
+        $scope.addColumn = function () {
+            //console.log($scope.tableName);
+            //console.log($scope.columnInfo.cName + " " + $scope.columnInfo.typeList + " " + $scope.columnInfo.length + " " + $scope.columnInfo.attributesList + " " + $scope.columnInfo.nullCKBox + " " + $scope.columnInfo.indexList + " " + $scope.columnInfo.aiCKBox);
+
+            var colName = $scope.tableName + $scope.columnInfo.cName;
+
+            // check there is duplicate column name or not
+            if ($.inArray(colName, columnList) > -1) {
+                alert($scope.columnInfo.cName + " already exists")
+            } else {
+
+                columnList.push(colName);
+                obj[$scope.tableName][$scope.columnInfo.cName] = {
+                    cName: $scope.columnInfo.cName,
+                    typeList: $scope.columnInfo.typeList,
+                    length: $scope.columnInfo.length,
+                    attributesList: $scope.columnInfo.attributesList,
+                    nullCKBox: $scope.columnInfo.nullCKBox,
+                    indexList: $scope.columnInfo.indexList,
+                    aiCKBox: $scope.columnInfo.aiCKBox
+                };
+                //console.log(obj);
+            }
+
+        }
+
+
+        $scope.$on('data_shared', function () {
+            var tName = dataShare.getData();
+            $scope.tableName = tName;
+        });
+    }
+]);
+
+app.factory('dataShare', function ($rootScope) {
+    var service = {};
+    service.data = false;
+    service.sendData = function (data) {
+        this.data = data;
+        $rootScope.$broadcast('data_shared');
+    };
+    service.getData = function () {
+        return this.data;
+    };
+    return service;
 });
--- js/helper.js
+++ js/helper.js
@@ -3,41 +3,99 @@
  */
 
 var tables = [];
-var tableNum = 1;
+var tableNum = 0;
 var obj = {}
 
-$("#add-tbl").click(function () {
+/*
+ $(document).on('click', "input.newBtn", function () {
+ console.log(this.id);
+ });
+ */
+$(document).on('click', "input.deleteBtn", function () {
 
-    // new table
-    var $element = $('<div class="draggableResizable col-md-3" id="tables"> ' +
-        '<input class="col-sm-6" id ="tempInput" type="text" placeholder="column name"> ' +
-        '<select class="col-sm-4">' +
-        '<option value="INT">INT</option>' +
-        '<option value="VARCHAR">VARCHAR</option>' +
-        '</select>'+
-        '<input class="col-sm-12" id="tempBtn" type="button" value="+"> ' +
-        '</div>');
+    // get num from deleteBtn's id
+    var num = this.id.replace(/^\D+/g, '');
 
+    // get table id
+    var table = 'table' + num;
 
-    // add table
-    obj.table_id = "table" + tableNum;
-    tables.push(obj);
+    // delete table
+    $('#' + table).remove();
 
-    tableNum = tableNum + 1;
+    // delete table from table list
+    var idx = tables.indexOf(table);
 
-    //append table
-    $("#table").append($element);
+    if (idx > -1) {
+        tables.splice(idx, 1);
+    }
 
-    //make it draggable and resizable
-    $element.draggable().resizable();
-
-    $("#tempInput").attr("id", "table" + tableNum);
-
-    return false;
+    // delete table from obj
+    delete obj[table];
+    console.log(obj);
 
 });
 
-console.log(tableNum);
+// delete column
+$(document).on('click', "input.deleteColumn", function () {
+
+    // get num from deleteBtn's id
+    var num = this.id.replace(/^\D+/g, '');
+    var cName = this.id.replace(/[0-9]/g, '');
+
+    // get table id
+    var table = 'table' + num
+
+    // delete column
+    $('#colDiv' + cName + num).remove();
+
+
+    // delete column from obj
+    delete obj[table][cName];
+    console.log(obj);
+
+});
+
+
+// add table
+$("#add-tbl").click(function () {
+
+    var tableName = $('#tableName').val();
+    var $element = $('<div class="draggableResizable tables" id="tempTable"> ' +
+        '<p id="tempP">' + tableName + '</p> ' +
+        '<input class="col-sm-12 newBtn" id="tempNEW" type="button" value="NEW"> ' +
+        '<input class="col-sm-12 deleteBtn" id="tempDLT" type="button" value="DELETE"> ' +
+        '</div>');
+
+    // check table name already exist or not
+    if ($.inArray(tableName, tables) > -1) {
+        alert(tableName + " already exists")
+    } else {
+
+        tables.push("table" + tableNum);
+
+        obj["table" + tableNum] = {
+            tableName: tableName,
+            newBtn: "newBtn" + tableNum,
+            deleteBtn: "deleteBtn" + tableNum
+        };
+
+        //append table
+        $("#table").append($element);
+
+        //make it draggable and resizable
+        $element.draggable().resizable();
+
+        $("#tempTable").attr("id", "table" + tableNum);
+        $("#tempNEW").attr("id", "newBtn" + tableNum);
+        $("#tempDLT").attr("id", "deleteBtn" + tableNum);
+        $("#tempP").attr("id", "p" + tableNum);
+
+        tableNum = tableNum + 1;
+
+    }
+
+});
+
 
 $("#generate").click(function () {
 
@@ -57,16 +115,219 @@ $("#generate").click(function () {
 });
 
 
+// dialog form
+$(function () {
 
+    var num;
+
+    var columnName;
+    var tableId;
+
+    function createTable() {
+        var cName = $('#cName').val();
+        var typeList = $('#typeList option:selected').text();
+        var length = $('#length').val();
+        var attributes = $('#attributesList option:selected').text();
+        var nullCKBox = $('#nullCKBox').is(':checked');
+        var indexList = $('#indexList option:selected').text();
+        var aiCKBox = $('#aiCKBox').is(':checked');
+
+
+        // check table already has same column's name or not
+        if (obj['table' + num][cName] == null) {
+            var $element = $('<div id="colDiv' + cName + num + '">' +
+                '<p>' + cName + ': ' + typeList + '</p>' +
+                '<input class="deleteColumn" id="' + cName + num + '" type="button" value="delete column">' +
+                '<input class="editColumn" id="' + num + cName + '" type="button" value="edit column">' +
+                '</div>');
+
+            //append table
+            $("#p" + num).append($element);
+
+            obj['table' + num][cName] = {
+                type: typeList,
+                length: length,
+                attributes: attributes,
+                nullCKBox: nullCKBox,
+                indexList: indexList,
+                aiCKBox: aiCKBox
+            };
+            dialog.dialog("close");
+        } else {
+            alert("this column's name already exists");
+        }
+
+
+        console.log(obj);
+        //console.log(cName + "," + typeList + "," + length + "," + attributes + "," + nullCKBox + "," + indexList + "," + aiCKBox);
+
+    }
+
+
+
+
+    function editColumn() {
+        var cName = $('#cName').val();
+        var typeList = $('#typeList option:selected').text();
+        var length = $('#length').val();
+        var attributes = $('#attributesList option:selected').text();
+        var nullCKBox = $('#nullCKBox').is(':checked');
+        var indexList = $('#indexList option:selected').text();
+        var aiCKBox = $('#aiCKBox').is(':checked');
+
+
+        // check table already has same column's name or not
+        if (obj['table' + num][cName] == null) {
+            var $element = $('<div id="colDiv' + cName + num + '">' +
+                '<p>' + cName + ': ' + typeList + '</p>' +
+                '<input class="deleteColumn" id="' + cName + num + '" type="button" value="delete column">' +
+                '<input class="editColumn" id="' + num + cName + '" type="button" value="edit column">' +
+                '</div>');
+
+            //append table
+            $("#p" + num).append($element);
+
+            obj['table' + num][cName] = {
+                type: typeList,
+                length: length,
+                attributes: attributes,
+                nullCKBox: nullCKBox,
+                indexList: indexList,
+                aiCKBox: aiCKBox
+            };
+            dialog.dialog("close");
+        } else {
+            alert("this column's name already exists");
+        }
+
+
+        console.log(obj);
+        //console.log(cName + "," + typeList + "," + length + "," + attributes + "," + nullCKBox + "," + indexList + "," + aiCKBox);
+
+    }
+
+
+
+    dialog = $("#dialog-form").dialog({
+        autoOpen: false,
+        height: 400,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Create table": createTable,
+            Cancel: function () {
+                dialog.dialog("close");
+            }
+        },
+        close: function () {
+            form[0].reset();
+        }
+    });
+
+    form = dialog.find("form").on("submit", function (event) {
+        event.preventDefault();
+
+    });
+
+
+    // add new column
+    $(document).on('click', "input.newBtn", function () {
+        num = this.id.replace(/^\D+/g, '');
+        dialog.dialog("open");
+        console.log("new");
+    });
+
+
+
+    editDialog = $("#dialog-form").dialog({
+        autoOpen: false,
+        height: 400,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Edit Column": editColumn,
+            Cancel: function () {
+                editDialog.dialog("close");
+            }
+        },
+        close: function () {
+            form[0].reset();
+        }
+    });
+
+
+    // edit column
+    $(document).on('click', "input.editColumn", function () {
+
+        // get num from deleteBtn's id
+        num = this.id.replace(/^\D+/g, '');
+        columnName = this.id.replace(/[0-9]/g, '');
+
+        // get table id
+        tableId = 'table' + num;
+
+
+        editDialog.dialog("open");
+
+
+        $('#cName').val(columnName);
+        $('#typeList option:selected').val(JSON.stringify(obj[tableId][columnName].type));
+        $('#length').val(obj[tableId][columnName].length);
+        $('#attributesList option:selected').val(obj[tableId][columnName].attributes);
+        $('#nullCKBox').val(obj[tableId][columnName].nullCKBox);
+        $('#indexList option:selected').val(obj[tableId][columnName].indexList);
+        $('#aiCKBox').val(obj[tableId][columnName].aiCKBox);
+
+
+    });
+
+
+
+    /*
+     $("#create-tbl").button().on("click", function () {
+     dialog.dialog("open");
+     });
+     */
+});
+
+
+// dropdown list
+
+var tableType = ['INT', 'CHARACTER', 'VARCHAR'];
+var attributes = ['', 'BINARY', 'UNSIGNED', 'UNSIGNED ZEROFILL', 'on update CURRENT_TIMESTAMP'];
+var indexType = ['', 'PRIMARY', 'UNIQUE'];
+
+//table type list
+$.each(tableType, function (key, value) {
+    $('#typeList').append($("<option></option>").attr("value", key).text(value));
+});
+
+// attributes type
+$.each(attributes, function (key, value) {
+    $('#attributesList').append($("<option></option>").attr("value", key).text(value));
+});
+
+// index type
+$.each(indexType, function (key, value) {
+    $('#indexList').append($("<option></option>").attr("value", key).text(value));
+});
+
+
+// cookie
 $("#test").click(function () {
 
-    obj.MyGuest={id:{datatype: "int", nullType: true, primary: true}, firstname:{datatype: "string", nullType: false, primary: false}};
+    obj.MyGuest = {
+        id: {datatype: "int", nullType: true, primary: true},
+        firstname: {datatype: "string", nullType: false, primary: false}
+    };
 
 
     console.log(obj);
 
+    console.log(obj.MyGuest.firstname.datatype);
+
     //save to cookie
-    $.cookie('obj',  JSON.stringify(obj));
+    $.cookie('obj', JSON.stringify(obj));
 
     //get cookie
     console.log(JSON.parse($.cookie('obj')));
@@ -76,73 +337,4 @@ $("#test").click(function () {
 
     //test
     console.log($.cookie('obj'));
-});
-
-
-
-
-
-// dialog form
-$( function() {
-
-    function createTable() {
-        var tName = $('#tName').val();
-        var typeList = $('#typeList option:selected').text();
-        var length = $('#length').val();
-        var attributes = $('#attributesList option:selected').text();
-        var nullCKBox = $('#nullCKBox').is(':checked');
-        var indexList = $('#indexList option:selected').text();
-        var aiCKBox = $('#aiCKBox').is(':checked');
-
-        console.log(tName + "," + typeList + "," + length + "," + attributes + "," + nullCKBox + "," + indexList + "," + aiCKBox);
-    }
-
-
-
-    dialog = $( "#dialog-form" ).dialog({
-        autoOpen: false,
-        height: 400,
-        width: 350,
-        modal: true,
-        buttons: {
-            "Create table" : createTable,
-            Cancel: function() {
-                dialog.dialog( "close" );
-            }
-        },
-        close: function() {
-            form[ 0 ].reset();
-        }
-    });
-
-    form = dialog.find( "form" ).on( "submit", function( event ) {
-        event.preventDefault();
-
-    });
-
-    $( "#create-tbl" ).button().on( "click", function() {
-        dialog.dialog( "open" );
-    });
-} );
-
-
-// dropdown list
-
-var tableType = ['INT', 'CHARACTER', 'VARCHAR'];
-var attributes = ['','BINARY', 'UNSIGNED', 'UNSIGNED ZEROFILL', 'on update CURRENT_TIMESTAMP'];
-var indexType = ['','PRIMARY', 'UNIQUE'];
-
-//table type list
-$.each(tableType, function(key, value) {
-    $('#typeList').append($("<option></option>").attr("value", key).text(value));
-});
-
-// attributes type
-$.each(attributes, function(key, value) {
-    $('#attributesList').append($("<option></option>").attr("value", key).text(value));
-});
-
-// index type
-$.each(indexType, function(key, value) {
-    $('#indexList').append($("<option></option>").attr("value", key).text(value));
 });
